1ab74db802cc2b41c886f4537b0a04a787b2c456
kieeps <kieeps@kieeps.com>
2023-08-18 22:08:13 +0200

    sshd and basic packages

--- CT104/configuration.nix
+++ CT104/configuration.nix
@@ -3,5 +3,12 @@
 {
   imports = [ <nixpkgs/nixos/modules/virtualisation/lxc-container.nix> ];
 
-  
+  ## Services
+  services.openssh.enable = true;
+
+  ## Packages
+  environment.systemPackages = with pkgs; [
+    git
+    wget
+  ];
 }
