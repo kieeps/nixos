1b1225d85f63fb222f1569a20b855bf9d191c697
kieeps <kieeps@kieeps.com>
2023-08-18 22:26:31 +0200

    added tailscale

--- CT104/configuration.nix
+++ CT104/configuration.nix
@@ -2,12 +2,14 @@
 
 {
   imports = [ <nixpkgs/nixos/modules/virtualisation/lxc-container.nix> ];
-
+  time.timeZone = "Europe/Stockholm";
   ## Services
   services.openssh.enable = true;
+  services.tailscale.enable = true;
 
   ## Packages
   environment.systemPackages = with pkgs; [
+    tailscale
     git
     wget
   ];
