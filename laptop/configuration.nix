2f62e24e28d9b4b27bead86c9110036cb0a97e48
Kieeps <kieeps@kieeps.com>
2023-11-04 10:11:32 +0100

    zhs fixes

--- laptop/configuration.nix
+++ laptop/configuration.nix
@@ -2,40 +2,66 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
+
 { config, pkgs, ... }:
 
 {
-  nixpkgs.config.allowUnfree = true;
-  nixpkgs.config.permittedInsecurePackages = [
-        "openssl-1.1.1u"
-		"python-2.7.18.6"
-              ];
-hardware.opengl.driSupport32Bit = true;
-hardware.pulseaudio.support32Bit = true;
-nixpkgs.config.allowUnfreePredicate = (pkg: builtins.elem (builtins.parseDrvName pkg.name).name [ "steam" ]);
-nix.settings = {
-    substituters = ["https://nix-gaming.cachix.org"];
-    trusted-public-keys = ["nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="];
-  };
-
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      <home-manager/nixos> 
     ];
 
-  ## Bootloader.
+
+#  programs.zsh.enable = true;
+
+
+  programs.zsh = {
+    enable = true;
+    enableGlobalCompInit = false;
+    ohMyZsh = {
+      enable = true;
+      theme = "bira";
+      customPkgs = with pkgs; [
+        zsh-git-prompt
+        zsh-nix-shell
+        zsh-completions
+        zsh-command-time
+        zsh-fast-syntax-highlighting
+        nix-zsh-completions
+      ];
+    };
+  };
+
+
+#  oh-my-zsh.enable = true;
+  users.defaultUserShell = pkgs.zsh;
+
+  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  # boot.loader.grub.theme = true;
+  boot.loader.grub.theme = true;
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  networking.hostName = "Lappen"; # Define your hostname.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  # Vulcan
+  hardware.opengl.driSupport = true;
+  # For 32 bit applications
+  hardware.opengl.driSupport32Bit = true;
 
-  ## Set your time zone.
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
   time.timeZone = "Europe/Stockholm";
 
-  ## Select internationalisation properties.
+  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
+
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "sv_SE.UTF-8";
     LC_IDENTIFICATION = "sv_SE.UTF-8";
@@ -48,9 +74,11 @@ nix.settings = {
     LC_TIME = "sv_SE.UTF-8";
   };
 
-  ## Shell
-  programs.zsh.enable = true;
-  users.defaultUserShell = pkgs.zsh;
+  # enable the tailscale service
+  services.tailscale.enable = true;
+
+  ## virtualisation
+  virtualisation.docker.enable = true;
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
@@ -68,149 +96,97 @@ nix.settings = {
   # Configure console keymap
   console.keyMap = "sv-latin1";
 
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.kieeps = {
-     isNormalUser = true;
-     extraGroups = [ "wheel" "kvm" "input" "disk" "libvirtd" ]; # Enable ‘sudo’ for the user.
+    isNormalUser = true;
+    description = "kieeps";
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    packages = with pkgs; [
+      kcalc
+      firefox
+      kate
+      discord
+      yakuake
+      steam
+      google-chrome
+      signal-desktop
+      vim
+      git
+    ];
   };
+
+  # Enable automatic login for the user.
+  services.xserver.displayManager.autoLogin.enable = true;
+  services.xserver.displayManager.autoLogin.user = "kieeps";
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-   environment.systemPackages = with pkgs; [
-	wget
-    neofetch
-	flatpak
-	gcc
-	git
-	github-desktop
-	gparted
-	libverto
-	lutris
-	nfs-utils
-	openssl
-	powershell
-	protonup-ng
-	python3Full
-	python.pkgs.pip
-	qemu
-	steam
-	steam-run
-	virt-manager
-	xclip
-    firefox
-    discord
-    yakuake
-    libreoffice
-    vscode
-    docker-compose
-    nextcloud-client
-    htop
-    radeontop
-    btop
-    zsh
-    oh-my-zsh
-    tailscale
-    element-desktop
-    tlp
-
-    # Overrides, Extra deps
-	(lutris.override {
-	       extraPkgs = pkgs: [
-		 wineWowPackages.stable
-		 winetricks
-	       ];
-	    })
+  environment.systemPackages = with pkgs; [
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  wget
   ];
 
-#  nixpkgs.overlays = [
-#	(final: prev: {
-#		dwm = prev.dwm.overrideAttrs (old: { src = /home/titus/GitHub/dwm-titus ;});
-#	})
-#  ];
-  
-  ## Gaming
-	programs.steam = {
-	  enable = true;
-	  remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-	  dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-	};
-	  
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
 
   # List services that you want to enable:
-  services.tailscale.enable = true;
-  virtualisation.libvirtd.enable = true; 
-  # enable flatpak support
-  services.flatpak.enable = true;
-  services.dbus.enable = true;
-  xdg.portal = {
-    enable = true;
-    # wlr.enable = true;
-    # gtk portal needed to make gtk apps happy
-    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-  };
-  security.polkit.enable = true;
- systemd = {
-  user.services.polkit-gnome-authentication-agent-1 = {
-    description = "polkit-gnome-authentication-agent-1";
-    wantedBy = [ "graphical-session.target" ];
-    wants = [ "graphical-session.target" ];
-    after = [ "graphical-session.target" ];
-    serviceConfig = {
-        Type = "simple";
-        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-        Restart = "on-failure";
-        RestartSec = 1;
-        TimeoutStopSec = 10;
-      };
-  };
-   extraConfig = ''
-     DefaultTimeoutStopSec=10s
-   '';
-}; 
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  networking.firewall.enable = false;
-  networking.enableIPv6 = false;
+  # networking.firewall.enable = false;
 
-fonts = {
-    fonts = with pkgs; [
-      noto-fonts
-      noto-fonts-cjk
-      noto-fonts-emoji
-      font-awesome
-      source-han-sans
-      source-han-sans-japanese
-      source-han-serif-japanese
-      (nerdfonts.override { fonts = [ "Meslo" ]; })
-    ];
-    fontconfig = {
-      enable = true;
-      defaultFonts = {
-	      monospace = [ "Meslo LG M Regular Nerd Font Complete Mono" ];
-	      serif = [ "Noto Serif" "Source Han Serif" ];
-	      sansSerif = [ "Noto Sans" "Source Han Sans" ];
-      };
-    };
-};
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  system.copySystemConfiguration = true;
-  system.autoUpgrade.enable = true;  
-  system.autoUpgrade.allowReboot = true; 
-  system.autoUpgrade.channel = "https://channels.nixos.org/nixos-23.05";
-  
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "22.11"; # Did you read the comment?
+environment.sessionVariables = {
+  MOZ_USE_XINPUT2 = "1";
+};
 
+
+  system.stateVersion = "23.05"; # Did you read the comment?
+
+home-manager.users.kieeps = { pkgs, config, ... }: {
+home.stateVersion = "23.05";  
+  home.packages = [  ];
+  };
 }
 
