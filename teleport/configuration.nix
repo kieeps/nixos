0adb986e3653c984b4a64f42b419d26df8bdd389
telenix <telenix@kieeps.com>
2024-08-06 11:04:44 +0200

    full configs for teleport

---
+++ teleport/configuration.nix
@@ -0,0 +1,86 @@
+{ inputs, lib, config, pkgs, ... }:
+
+{
+  imports = [ <nixpkgs/nixos/modules/virtualisation/lxc-container.nix> ];
+  time.timeZone = "Europe/Stockholm";
+
+  ## Supress systemd units that don't work because of LXC
+  systemd.suppressedSystemUnits = [
+    "dev-mqueue.mount"
+    "sys-kernel-debug.mount"
+    "sys-fs-fuse-connections.mount"
+  ];
+
+### System packages
+  environment.systemPackages = with pkgs; [
+    wget
+    git
+    btop
+    screen
+  ];
+
+
+  nixpkgs = {
+    overlays = [
+    ];
+
+    config = {
+      allowUnfree = true;
+    };
+  };
+
+  nix = {
+    settings = {
+      experimental-features = "nix-command flakes";
+      auto-optimise-store = true;
+    };
+  };
+
+  networking.hostName = "teleport";
+
+  users.users = {
+    kieeps = {
+      initialPassword = "kieeps2win";
+      isNormalUser = true;
+      uid = 1000;
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOltNlDAwXAw2mfdyMtdleOCfPQB2GSQ9l5W9cKbg+5i supern@kieeps.com"
+        "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIA5UjtYVIaojFVR95EUqe2jwpR25auHVRnhTa+5HrysEAAAABHNzaDo= kieeps@Lappen"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJJ+BbPDmvbxe8F0PRqJb8eQ/S0PFvHpd21/3W3t1zXe obelix@kieeps.com"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFtSbMFj9DxS5nb3F2tDHRNpw9UxRmIBYLaZvgydIu0r root@HomeSense.local"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBvlBIhC5sxCAP0wzdKV23efXXPQ7P2ToxJdxQEmChr4 JuiceSSH"
+      ];
+      extraGroups = [ "wheel" ];
+    };
+  };
+
+    users.users = {
+    root = {
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL+dFVrzTCoGKIzTdazKVFmmQWc0tGTcj35EewEwlcyL ansible@kieeps.com"
+      ];
+      extraGroups = [ "root" ];
+    };
+  };
+
+
+  programs.bash.shellAliases = {
+  la = "ls -als";
+  ls = "ls --color=tty";
+  };
+
+
+  ## Services
+  services.teleport = { 
+    enable = true;
+    package = pkgs.teleport;
+  };
+
+  services.openssh = {
+    enable = true;
+    settings.PasswordAuthentication = true;
+    settings.KbdInteractiveAuthentication = false;
+    settings.PermitRootLogin = "yes";
+ };
+  system.stateVersion = "23.05";
+}
