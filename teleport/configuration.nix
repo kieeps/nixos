434ac4c160f3a149a4c9d1aeb092bef51cb9b6eb
kieeps <kieeps@kieeps.com>
2024-08-06 17:15:55 +0200

    changed datadir

--- teleport/configuration.nix
+++ teleport/configuration.nix
@@ -80,7 +80,7 @@
     settings = {
       teleport = {
         nodename = "teleport-main";
-        # data_dir =  /var/lib/teleport;
+        data_dir =  /root/teleport;
 
       };
       auth_service = {
